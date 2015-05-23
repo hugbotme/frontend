558a01c221f69369b11ab809239850a301a02a48
creinartz <christoph.reinartz@trivago.com>
2015-05-24 00:02:33 +0200

    add assets

--- app/js/site.js
+++ app/js/site.js
@@ -1,3 +1,28 @@
 if (!Modernizr.touch) {
     var s = skrollr.init({forceHeight: false});
 }
+
+$(document).ready(function() {
+    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    getData(chart);
+    setInterval(getData(chart),300);
+});
+
+// http://www.hugbot.me/info.json
+
+
+function getData(chart) {
+    $.getJSON("http://localhost/~creinartz/hugbot-frontend/app/test.json", function(data) {
+        $('.js_tweet-count').html(data.received);
+        var data = google.visualization.arrayToDataTable([
+            ['Total', 'Status'],
+            ['In Progress',      data.in_progress],
+            ['Merged',      data.merged],
+            ['Closed',  data.closed]
+        ]);
+        var options = {
+        };
+        chart.draw(data, options);
+    });
+}
+
